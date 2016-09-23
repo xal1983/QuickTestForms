3b18e9a3fef9686fc1b46b4552cdac128b011556
andres <andres@DESKTOP-10BRGV1>
2016-09-22 23:24:15 -0300

    Add project files.

---
+++ QuickTestForms/QuickTestForms/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace QuickTestForms
+{
+  public partial class MainPage : ContentPage
+  {
+    public MainPage()
+    {
+      InitializeComponent();
+    }
+
+    int x;
+    void Button_Clicked(object sender, EventArgs e)
+    {
+      lblResult.Text = $"Button clicked {++x} times.{Environment.NewLine}Two lines.";
+      if (Device.OS == TargetPlatform.iOS)
+        iosLabel.Text = lblResult.Text;
+
+      
+    }
+  }
+}
